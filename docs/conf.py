b2809a82e9cc238696cc93bfab97fb96bde74f22
Shihab Suliman <rye74444@cs03r-sc-serv-44.diamond.ac.uk>
2024-05-19 18:55:25 +0100

    redocs

--- docs/conf.py
+++ docs/conf.py
@@ -12,6 +12,11 @@ import requests
 
 import pandablocks
 
+require("sphinx_rtd_theme")
+require("matplotlib")
+ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, ROOT)
+
 # -- General configuration ------------------------------------------------
 
 # General information about the project.
