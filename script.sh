5458a08afb6b96e8e57e795cb84a3be39db6b0c3
Shihab Suliman <rye74444@ws557.diamond.ac.uk>
2024-05-20 11:13:44 +0100

    bash

---
+++ script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+TOP=$(pwd)
+mkdir -p $TOP/docs/build	
+DOCS_BUILD_DIR=$TOP/docs/build
+ALL_RST_FILES_UNSORTED=$(find docs modules -name "*.rst")
+ALL_RST_FILES=$(echo "$ALL_RST_FILES_UNSORTED" | tr ' ' '\n' | sort)
+BUILD_RST_FILES_UNSORTED=$(find $DOCS_BUILD_DIR -name "*.rst")
+BUILD_RST_FILES=$(echo "$BUILD_RST_FILES_UNSORTED" | tr ' ' '\n' | sort)
+SRC_RST_FILES=$(comm -3 <(echo "$BUILD_RST_FILES") <(echo "$ALL_RST_FILES"))
+sphinx-build -b html -c ./docs/ docs $TOP/../build/html
+sphinx-{posargs:build -EW --keep-going} -T $TOP/../build/html
