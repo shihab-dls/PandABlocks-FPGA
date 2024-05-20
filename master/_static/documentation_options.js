e19cfbe222a82e276cc29980b3846b055384c7e5
shihab-dls <shihab-dls@users.noreply.github.com>
2024-05-20 11:06:10 +0000

    deploy: e47ff0e01fe8b8bfdf94374d8d20034d1aeb2e70

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3f59bb6',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '0.1.dev2594+ge47ff0e',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
