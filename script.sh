e47ff0e01fe8b8bfdf94374d8d20034d1aeb2e70
Shihab Suliman <rye74444@ws557.diamond.ac.uk>
2024-05-20 12:04:18 +0100

    cd

--- script.sh
+++ script.sh
@@ -7,4 +7,4 @@ ALL_RST_FILES=$(echo "$ALL_RST_FILES_UNSORTED" | tr ' ' '\n' | sort)
 BUILD_RST_FILES_UNSORTED=$(find $DOCS_BUILD_DIR -name "*.rst")
 BUILD_RST_FILES=$(echo "$BUILD_RST_FILES_UNSORTED" | tr ' ' '\n' | sort)
 SRC_RST_FILES=$(comm -3 <(echo "$BUILD_RST_FILES") <(echo "$ALL_RST_FILES"))
-sphinx-build -b html -c ./docs/ docs $TOP/../build/html
+sphinx-build -b html -c ./docs/ docs $TOP/build/html
